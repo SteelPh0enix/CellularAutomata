3a19f7e4ff2e2d783280f83f6bf566bbf5c84d8f
SteelPh0enix <wojciech_olech@hotmail.com>
2025-03-22 00:39:20 +0100

    un-constexpr'd setup_window

--- lib/Window/Window.hpp
+++ lib/Window/Window.hpp
@@ -18,26 +18,4 @@ struct WindowConfig {
     std::optional<int> monitor;
 };
 
-constexpr void setup_window(WindowConfig const& config) {
-    InitWindow(config.minimalSize.width, config.minimalSize.height, config.title.c_str());
-
-    auto const selectedMonitor =
-        config.monitor.has_value() ? config.monitor.value() : GetCurrentMonitor();
-    auto const maxFramerate =
-        config.maxFps.has_value() ? config.maxFps.value() : GetMonitorRefreshRate(selectedMonitor);
-    auto const selectedMonitorWidth = GetMonitorWidth(selectedMonitor);
-    auto const selectedMonitorHeight = GetMonitorHeight(selectedMonitor);
-
-    auto const appWindowWidth =
-        static_cast<int>(static_cast<double>(selectedMonitorWidth) * config.defaultSizeMultiplier);
-    auto const appWindowHeight =
-        static_cast<int>(static_cast<double>(selectedMonitorHeight) * config.defaultSizeMultiplier);
-    auto const appWindowX = (selectedMonitorWidth - appWindowWidth) / 2;
-    auto const appWindowY = (selectedMonitorHeight - appWindowHeight) / 2;
-
-    SetWindowMonitor(selectedMonitor);
-    SetWindowMinSize(config.minimalSize.width, config.minimalSize.height);
-    SetWindowSize(appWindowWidth, appWindowHeight);
-    SetWindowPosition(appWindowX, appWindowY);
-    SetTargetFPS(maxFramerate);
-}
+void setup_window(WindowConfig const& config);
