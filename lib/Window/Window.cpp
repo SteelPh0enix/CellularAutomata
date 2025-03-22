7d75d4dae5d122b80328dd979c362ffd3b9ef650
SteelPh0enix <wojciech_olech@hotmail.com>
2025-03-22 13:45:43 +0100

    Fixed window positioning

--- lib/Window/Window.cpp
+++ lib/Window/Window.cpp
@@ -1,5 +1,7 @@
 #include "Window.hpp"
 
+#include <spdlog/spdlog.h>
+
 void setup_window(WindowConfig const& config) {
     InitWindow(config.minimalSize.width, config.minimalSize.height, config.title.c_str());
 
@@ -17,9 +19,20 @@ void setup_window(WindowConfig const& config) {
     auto const appWindowX = (selectedMonitorWidth - appWindowWidth) / 2;
     auto const appWindowY = (selectedMonitorHeight - appWindowHeight) / 2;
 
+    auto const currentMonitorPosition = GetMonitorPosition(selectedMonitor);
+    auto const appWindowGlobalX = appWindowX + currentMonitorPosition.x;
+    auto const appWindowGlobalY = appWindowY + currentMonitorPosition.y;
+
+    spdlog::info("Selected monitor {}, {} FPS, {}x{}", selectedMonitor, maxFramerate,
+                 selectedMonitorWidth, selectedMonitorHeight);
+    spdlog::info("Current monitor position: {}x{}", currentMonitorPosition.x,
+                 currentMonitorPosition.y);
+    spdlog::info("Creating a window with dimensions {}x{} at {}, {}", appWindowWidth,
+                 appWindowHeight, appWindowGlobalX, appWindowGlobalY);
+
     SetWindowMonitor(selectedMonitor);
     SetWindowMinSize(config.minimalSize.width, config.minimalSize.height);
     SetWindowSize(appWindowWidth, appWindowHeight);
-    SetWindowPosition(appWindowX, appWindowY);
+    SetWindowPosition(appWindowGlobalX, appWindowGlobalY);
     SetTargetFPS(maxFramerate);
 }
